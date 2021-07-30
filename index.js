37cd9b17c898e1350614655a6f3c9fbf3653a66e
goffle <se.legoff@gmail.com>
2021-07-30 08:05:07 +0200

    auto find start date add multipe project add table creation

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+require("dotenv").config();
 const PiwikClient = require("piwik-client");
 const { Client } = require("pg");
 
-const START_DATE = process.env.START_DATE || "2021-06-01";
-const DESTINATION_TABLE = process.env.DESTINATION_TABLE || "matomo";
-const MATOMO_KEY = process.env.MATOMO_KEY;
-const MATOMO_SITEID = process.env.MATOMO_SITEID;
+const MATOMO_KEY = process.env.MATOMO_KEY || "";
+const DEFAULTSTARTDATE = "2020-01-01";
 const MATOMO_URL = process.env.MATOMO_URL || "https://matomo.fabrique.social.gouv.fr/";
-
 const RESULTPERPAGE = 20000;
+const connectionString = process.env.PGDATABASE || "";
 
-const startDate = new Date(START_DATE);
-const connectionString = process.env.PGDATABASE;
+const client = new Client({ connectionString });
+
+(async () => {
+  await client.connect();
+  await run(37, "msp");
+  await run(22, "oz");
+})();
 
 function getDaysArray(s, e) {
   for (var a = [], d = new Date(s); d <= e; d.setDate(d.getDate() + 1)) {
@@ -19,20 +23,17 @@ function getDaysArray(s, e) {
   return a;
 }
 
-function getPreviousDate(days) {
-  const d = new Date();
-  d.setDate(d.getDate() - days);
-  return d;
-}
+async function run(siteId, project) {
+  const m = new Matomo(MATOMO_URL, MATOMO_KEY, siteId);
 
-(async () => {
-  const client = new Client({ connectionString });
-  await client.connect();
-  console.log("START");
+  await createTable(project);
 
-  const m = new Matomo(MATOMO_URL, MATOMO_KEY, MATOMO_SITEID);
+  const arr = (await client.query(`SELECT * FROM ${project} ORDER BY action_timestamp DESC  LIMIT 1;`)).rows;
 
-  const dates = getDaysArray(new Date(startDate), new Date());
+  let startDate = arr.length ? new Date(arr[0].action_timestamp) : new Date(DEFAULTSTARTDATE);
+  startDate.setDate(startDate.getDate() - 3); // We remove 3 days, to be sure
+
+  const dates = getDaysArray(startDate, new Date());
   dates.pop();
 
   // FIND LAST DAY SCRAPPED
@@ -46,13 +47,14 @@ function getPreviousDate(days) {
       filter_limit: 1,
       filter_sort_order: "asc",
     });
+    if (!visits.length) continue;
+
     const events = getEvents(visits[0]);
     const event = events[0];
     const lastEvent = (
-      await client.query(
-        `select * from ${DESTINATION_TABLE} where action_id=$1 order by action_timestamp desc limit 1`,
-        [event.action_id]
-      )
+      await client.query(`select * from ${project} where action_id=$1 order by action_timestamp desc limit 1`, [
+        event.action_id,
+      ])
     ).rows[0];
     currentIndex = o;
     if (!lastEvent) break;
@@ -80,10 +82,9 @@ function getPreviousDate(days) {
         try {
           const event = events[j];
           const lastEvent = (
-            await client.query(
-              `select * from ${DESTINATION_TABLE} where action_id=$1 order by action_timestamp desc limit 1`,
-              [event.action_id]
-            )
+            await client.query(`select * from ${project} where action_id=$1 order by action_timestamp desc limit 1`, [
+              event.action_id,
+            ])
           ).rows[0];
           if (lastEvent) {
             console.log(`SKIP ${event.action_id} ${dates[o]} (${i}/${visits.length})`);
@@ -92,8 +93,8 @@ function getPreviousDate(days) {
 
           console.log(`DOING ${event.action_id} (${i}/${visits.length})`);
 
-          const text = `insert into ${DESTINATION_TABLE}
-        (idSite, idVisit, actions, country, region, city, operatingSystemName, deviceModel, deviceBrand, visitDuration, daysSinceFirstVisit, visitorType, siteName, userId, serverDatePrettyFirstAction, action_id, action_type, action_eventCategory, action_eventAction, action_eventName, action_eventValue,action_timeSpent, action_timestamp, userCustomProperties)
+          const text = `insert into ${project}
+        (idsite, idvisit, actions, country, region, city, operatingsystemname, devicemodel, devicebrand, visitduration, dayssincefirstvisit, visitortype, sitename, userid, serverdateprettyfirstaction, action_id, action_type, action_eventcategory, action_eventaction, action_eventname, action_eventvalue,action_timespent, action_timestamp, usercustomproperties)
         values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)`;
 
           const value = Object.keys(event).map((e) => event[e]);
@@ -104,45 +105,76 @@ function getPreviousDate(days) {
       }
     }
   }
-})();
+}
 
 function getEvents(visit) {
   const arr = [];
   for (let j = 0; j < visit.actionDetails.length; j++) {
     const obj = {};
-    obj.idSite = visit.idSite;
-    obj.idVisit = visit.idVisit;
+    obj.idsite = visit.idSite;
+    obj.idvisit = visit.idVisit;
     obj.actions = visit.actions;
 
     obj.country = visit.country;
     obj.region = visit.region;
     obj.city = visit.city;
-    obj.operatingSystemName = visit.operatingSystemName;
-    obj.deviceModel = visit.deviceModel;
-    obj.deviceBrand = visit.deviceBrand;
-    obj.visitDuration = visit.visitDuration;
-    obj.daysSinceFirstVisit = visit.daysSinceFirstVisit;
-    obj.visitorType = visit.visitorType;
-    obj.siteName = visit.siteName;
-    obj.userId = visit.userId;
-    obj.serverDatePrettyFirstAction = new Date(visit.serverDatePrettyFirstAction);
+    obj.operatingsystemname = visit.operatingSystemName;
+    obj.devicemodel = visit.deviceModel;
+    obj.devicebrand = visit.deviceBrand;
+    obj.visitduration = visit.visitDuration;
+    obj.dayssincefirstvisit = visit.daysSinceFirstVisit;
+    obj.visitortype = visit.visitorType;
+    obj.sitename = visit.siteName;
+    obj.userid = visit.userId;
+    obj.serverdateprettyfirstaction = new Date(visit.serverDatePrettyFirstAction);
     obj.action_id = `${visit.idVisit}_${j}`;
     obj.action_type = visit.actionDetails[j].type;
-    obj.action_eventCategory = visit.actionDetails[j].eventCategory;
-    obj.action_eventAction = visit.actionDetails[j].eventAction;
-    obj.action_eventName = visit.actionDetails[j].eventName;
-    obj.action_eventValue = visit.actionDetails[j].eventValue;
-    obj.action_timeSpent = visit.actionDetails[j].timeSpent;
+    obj.action_eventcategory = visit.actionDetails[j].eventCategory;
+    obj.action_eventaction = visit.actionDetails[j].eventAction;
+    obj.action_eventname = visit.actionDetails[j].eventName;
+    obj.action_eventvalue = visit.actionDetails[j].eventValue;
+    obj.action_timespent = visit.actionDetails[j].timeSpent;
     obj.action_timestamp = new Date(visit.actionDetails[j].timestamp * 1000);
-    obj.userCustomProperties = {};
+    obj.usercustomproperties = {};
     for (let k = 1; k < 10; k++) {
       const property = visit.customVariables && visit.customVariables[`${j}`];
       if (!property) continue; // max 10 custom variables
-      obj.userCustomProperties[property[`customVariableName${k}`]] = property[`customVariableValue${k}`];
+      obj.usercustomproperties[property[`customVariableName${k}`]] = property[`customVariableValue${k}`];
     }
     arr.push(obj);
   }
   return arr;
+}
+
+async function createTable(project) {
+  const text = `CREATE TABLE IF NOT EXISTS ${project}
+  (
+    idsite                      text,
+    idvisit                     text,
+    actions                     text,
+    country                     text,
+    region                      text,
+    city                        text,
+    operatingsystemname         text,
+    devicemodel                 text,
+    devicebrand                 text,
+    visitduration               text,
+    dayssincefirstvisit         text,
+    visitortype                 text,
+    sitename                    text,
+    userid                      text,
+    serverdateprettyfirstaction date,
+    action_id                   text,
+    action_type                 text,
+    action_eventcategory        text,
+    action_eventaction          text,
+    action_eventname            text,
+    action_eventvalue           text,
+    action_timespent            text,
+    action_timestamp            date,
+    usercustomproperties        json
+)`;
+  await client.query(text);
 }
 
 class Matomo {
