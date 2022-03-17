fab9db7b24acef85d1478916b99c29b3eb187036
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-03-17 01:46:20 +0100

    fix: rename cleanup

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,6 @@ if (require.main === module) {
   const date =
     (process.argv[process.argv.length - 1].match(/^\d\d\d\d-\d\d-\d\d$/) && process.argv[process.argv.length - 1]) ||
     "";
-  console.log(`\nRunning @socialgouv/matomo-metabase ${date}\n`);
+  console.log(`\nRunning @socialgouv/matomo-postgres ${date}\n`);
   run(date);
 }
