140310c973c6e7a96475c14ba86f424f029493b7
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-06-21 02:58:50 +0200

    migrate on npx run

--- bin/index.js
+++ bin/index.js
@@ -7,5 +7,8 @@ if (require.main === module) {
     (process.argv[process.argv.length - 1].match(/^\d\d\d\d-\d\d-\d\d$/) && process.argv[process.argv.length - 1]) ||
     "";
   console.log(`\nRunning @socialgouv/matomo-postgres ${date}\n`);
+  console.log(`\nRunning migrations\n`);
+  require("../dist/migrate-latest");
+  console.log(`\nStarting import\n`);
   run(date);
 }
